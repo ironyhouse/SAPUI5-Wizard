b4ceb0599d91cef682f57156a7587b0e8f328c55
ironyhouse <36641168+ironyhouse@users.noreply.github.com>
2021-02-18 15:15:55 +0300

    feat: add comment

--- webapp/controller/Wizard.controller.js
+++ webapp/controller/Wizard.controller.js
@@ -1,29 +1,41 @@
 sap.ui.define(
   ["sap/ui/core/mvc/Controller"],
-  /**
-   * @param {typeof sap.ui.core.mvc.Controller} Controller
-   */
+  
   function (Controller) {
     "use strict";
 
     return Controller.extend("wizard.controller.Wizard", {
       onInit: function () {
 
-		// Finish Button text
-		this.getView().byId("CreateProductWizard").setFinishButtonText("Send (setFinishButtonText)")
-      },
+      // set Finish Button text
+      this.getView().byId("CreateProductWizard").setFinishButtonText("Send (setFinishButtonText)")
+    },
 
-	  onSendButtonPress: function () {
-		console.log(this.getView().getModel("State").getProperty("/"));
-      },
+    onDescriptionChange: function () {
+      var bValue = !!this.getView().getModel("State").getProperty("/productDescription").length;
+
+      // step number
+      console.log(this.getView().byId("CreateProductWizard").getProgress());
+
+      // go to next step
+      this.getView().byId("CreateProductWizard").nextStep();
+
+      // show next button
+      this.getView().byId("Step2").setValidated(bValue);
+    },
 
 	  onCompleteStep2: function () {
-		console.log("Complete Step 2");
-      },
+		  console.log("Complete Step 2");
+    },
 
 	  onCompleteStep3: function () {
-		console.log("Complete Step 3");
-      },
+		  console.log("Complete Step 3");
+    },
+
+    // if finish button press
+    onSendButtonPress: function () {
+      console.log(this.getView().getModel("State").getProperty("/"));
+    },
     });
   }
 );
